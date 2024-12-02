70249a2b49d78eb90ca62b2c12cb5170b5263896
Nicolas Beaussart <nic.beaussart@gmail.com>
2024-12-02 19:49:51 +0100

    Initial commit

---
+++ apps/nx-storybook-dynamic-ports/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="nx-storybook-dynamic-ports" />
+    </div>
+  );
+}
+
+export default App;
